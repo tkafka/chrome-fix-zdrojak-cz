b8b090d3763cfb079afca390d8a83b87be298b24
Tomas Kafka <tk@tomaskafka.com>
2013-03-02 18:35:30 +0100

    Adding zdrojak comments into main article :)

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,29 @@ chrome.extension.sendMessage({}, function(response) {
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/main.js");
-
-
+		// console.log("Hello. This message was sent from scripts/main.js");
 
 		// ----------------------------------------------------------
+
+		if (document.location.hostname == "www.zdrojak.cz") {
+			$('.entry-social').hide();
+			$('.entry-meta').hide();
+
+			var $allComments = $('a.all-comments');
+			if ($allComments) {
+				var documentUrl = document.URL;
+				var commentsUrl = documentUrl + $allComments.attr('href');
+
+				console.log('commentsUrl:', commentsUrl);
+
+				$.get(commentsUrl, function(data, textStatus, jqXHR) {
+					var $data = $(data);
+					var $fullComments = $data.find('#comments');
+
+					$('#comments').html($fullComments.html());
+				});
+			}
+		}
 
 	}
 	}, 10);
